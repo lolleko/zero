814b3a47271aeada87fe5912d803b8dfc21ed497
Lolleko <larry.junglas@hotmail.de>
2014-12-02 21:32:19 +0100

    fixed some scale issues

--- js/animation.js
+++ js/animation.js
@@ -2,13 +2,27 @@ jQuery(document).ready(function($) {
 
 	var headerInner = $('#header-inner');
 	headerInner.css('margin-left', $( document ).width()/2 - headerInner.width()/2);
-	$('#sidebar-cntnr').css('height', $('#content-cntnr').height()-210);
 
 	var container = document.querySelector('#content-cntnr');
 	var msnry = new Masonry( container, {
 	  	// options...
 	  	itemSelector: '.post-default'
 	});
+
+	/*$('.post-default').each(function( index ) {
+		var postH = $( this ).height();
+		console.log(index + ': ' + postH);
+		$('.post-information').each(function( indexi ) {
+			if (postH <= 300) {
+				console.log(indexi + " :" + $(this).height())
+				$('.post-information').eq(index).css('display','none');
+				$('.post').eq(index).css('width','728px');
+				$('.post').eq(index).css('margin-left','1px');
+				$('.post').eq(index).css('border-left','4px #d74e3e solid');
+
+			}
+		});
+	});*/
 
 	$(window).scroll(function(){
   		
@@ -22,7 +36,6 @@ jQuery(document).ready(function($) {
 	$(window).on('resize', function(){
 		var headerInner = $('#header-inner');
 		headerInner.css('margin-left', $( document ).width()/2 - headerInner.width()/2);
-		$('#sidebar-cntnr').css('height', $('#content-cntnr').height()-210);
 	});
 });
 
