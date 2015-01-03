b14435b7c4b2c6e2e6825314baaf55990859315b
Lolleko <larry.junglas@hotmail.de>
2015-01-03 17:49:52 +0100

    Added full support for link formats and some icons

--- content-link.php
+++ content-link.php
@@ -4,16 +4,6 @@
 				 		<div class="content-entry">
 				 			<?php the_content(); ?>
 				 		</div>
-				 		<div class="post-link-footer-cntnr">
-				 			<div class="link-author-cntnr">
-								<div class="link-ainfo-cntnr">
-									<?php the_author_posts_link(); ?>
-								</div>
-							</div>
-							<div class="link-dinfo-cntnr">
-								<?php echo get_the_date(); ?>
-							</div>
-				 		</div>
 				 	</div>
 			 	</div>
 			</div>
--- functions.php
+++ functions.php
@@ -189,7 +189,7 @@ function zero_register_menu() {
 add_action( 'init', 'zero_register_menu' );
 
 function modify_read_more_link() {
-    return '<a class="more-link" href="' . get_permalink() . '">&#8627;</a>';
+    return '<a class="more-link" href="' . get_permalink() . '"> ...</a>';
 }
 add_filter( 'the_content_more_link', 'modify_read_more_link' );
 
