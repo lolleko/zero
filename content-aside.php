e84d790d8ce360b0ca09e1b9e4bd30ec75208bb8
Lolleko <larry.junglas@hotmail.de>
2015-03-05 20:57:54 +0100

    Icons, Icons, Icons

--- content-aside.php
+++ content-aside.php
@@ -3,7 +3,10 @@
                         <a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>" class="entry-header-author-avatar">
                             <?php echo get_avatar( get_the_author_meta( 'email' ), '64' ); ?>
                         </a>
-                        <div class="entry-title" style="display:inline-block;"><?php the_author_posts_link(); ?></div>
+                        <div class="entry-title" style="display:inline-block;">
+                            <a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php the_title_attribute(); ?>">
+                                <?php the_author(); ?></a>
+                            </div>
                         <div class="entry-date-aside">
                             <?php echo get_the_date(); ?>
                         </div>
