b5e43d2da1af1c945b30096b6dbe4abf7c0007fe
Lolleko <larry.junglas@hotmail.de>
2015-03-05 20:08:29 +0100

    mobile optimzation doesn't require jquery anymore

--- js/animation.js
+++ js/animation.js
@@ -1,24 +1,27 @@
 jQuery(document).ready(function ($) {
 
 	$container = $('#page-cntnr');
-        $container.imagesLoaded(function(){
-            $container.masonry({
-                itemSelector: '.post',
-                "gutter": 40
-            });
 
-            $(".post").fadeIn(0, function(){
-                $(".post").fadeTo('fast', 1);
-                mobileOpt();
-            });
+    $container.imagesLoaded(function(){
+        $container.masonry({
+            itemSelector: '.post',
+            "gutter": 40
         });
+
+        $(".post").fadeIn(0, function(){
+            $(".post").fadeTo('fast', 1);
+            $container.masonry();
+        });
+    });
 
     $(window).load(function () {
         $container.masonry();
     });
 
 	$(window).resize(function () {
-        mobileOpt();
+        $container.imagesLoaded(function(){
+            $container.masonry();
+        });
 	});
 
     $( document.body ).on( 'post-load', function () {
@@ -34,16 +37,6 @@ jQuery(document).ready(function ($) {
 
         $container.masonry( 'reloadItems' );
     } );
-
-    function mobileOpt() {
-        docWidth = $(window).width();
-        if (docWidth <= 320) {
-            $('#sidebar-inner').css('width','100%');
-        }else{
-            $('#sidebar-inner').css('width','320');
-        }
-        $container.masonry();
-    }
 
     $('#header-menu').toggle(function(){
         $('#sidebar-cntnr').css('display', 'block');
