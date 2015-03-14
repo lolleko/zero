c78bab0505b46d7e100614ed708eb1e7db659f93
Lolleko <larry.junglas@hotmail.de>
2015-03-14 21:09:24 +0100

    dont display inactive footer

--- footer.php
+++ footer.php
@@ -1,7 +1,9 @@
 			</div>
+<?php if (is_active_sidebar('footer_widget_area')) : ?>
             <div id="footer-cntnr">
 <?php get_sidebar( 'footer' ); ?>
             </div>
+<?php endif; ?>
 		</div>
 		<?php wp_footer(); ?>
 	</body>
